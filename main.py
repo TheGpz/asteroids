b5c16448515c7509fcc22f22ac75cf99a1cc5c94
TheGpz <dstrogilos@gmail.com>
2025-02-19 09:24:46 +0100

    B: base loop main

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    i = 1
+    while i != 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
