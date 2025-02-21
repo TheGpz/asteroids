750c27d67e1fdb08a957aaa25de5202f308b55f1
TheGpz <dstrogilos@gmail.com>
2025-02-21 10:42:14 +0100

    E: comments

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
+    #Création des groupes
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #Ajout joueur aux groupes
+    Player.containers = (updatable, drawable)
+    #Init joueur
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
+    #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         
+        #Limite 60 FPS
         dt = clock.tick(60) / 1000
         
 if __name__ == "__main__":
